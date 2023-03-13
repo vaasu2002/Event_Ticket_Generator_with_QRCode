672e3a0a2cf295e1d63e328a0179e5371b8aca89
foundanand <anand.lahoti2021@vitbhopal.ac.in>
2023-03-14 01:08:54 +0530

    organised

--- QRBooking/database.py
+++ QRBooking/database.py
@@ -7,15 +7,16 @@ DATABASE_NAME = 'QREventBooking'
 COLLECTION_NAME = 'Test1'
 MONGODB_URL = "mongodb+srv://vaasu:pcvaasu9dps@cluster0.wydi0u7.mongodb.net/13digital?retryWrites=true&w=majority"
 
-def connectToDatabase()->pymongo.collection.Collection:
+
+def connectToDatabase() -> pymongo.collection.Collection:
     try:
-    
+
         client = pymongo.MongoClient(MONGODB_URL)
         collection = None
 
         if COLLECTION_NAME in client[DATABASE_NAME].list_collection_names():
-                collection = client[DATABASE_NAME][COLLECTION_NAME]        
-            
+            collection = client[DATABASE_NAME][COLLECTION_NAME]
+
         else:
             print("Collection does not exist")
             return None
@@ -23,6 +24,6 @@ def connectToDatabase()->pymongo.collection.Collection:
         if collection is not None:
             collection.create_index('registration_number', unique=True)
             return collection
-        
+
     except:
         print("Failed to connect to database")
