efba8e25d4ecfa53ede4c983c39788dee647fbd1
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-03-29 07:01:49 +0530

    update

--- QRBooking/generateQR/__init__.py
+++ QRBooking/generateQR/__init__.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import qrcode
 import pandas as pd
-from fpdf import FPDF
-import pyqrcode
-import png
-import os
+from QRBooking.generateQR.mail import send_ticket
 
-FILEPATH = "artifact/payment_data/payment_links_excel.xlsx"
+FILEPATH = "artifact\payment_data\payment_links_excel.xlsx"
 
 from QRBooking.database import connectToDatabase
 from QRBooking.generateQR.pdf import PDF
@@ -38,24 +35,31 @@ def make_qr(full_name:str,registration_number:str,unique_id:str,event_name:str):
 
 def generate_pdf(event_name:str,full_name:str,role:str,payment_status:str,
                     unique_id:str,qrcode_dir:str,registration_number:str,date_time_event='28 February 2023, 4PM to 7PM',venue_event='location_hehe'):
+    
+    try:
 
-    pdf = PDF(event_img_filepath=f"artifact/Event/{event_name}/logo/logo.png")
-    pdf.alias_nb_pages()
-    pdf.add_page()
-    pdf.set_font('Times', '', 12)
-    pdf.heading(EVENT_NAME=event_name)
-    pdf.date_time_venue(DATE_TIME_EVENT=date_time_event, VENUE_EVENT=venue_event)
-    pdf.attendee_name(PER_NAME=full_name)
-    pdf.role_in_event(ROLE=role)
-    pdf.payment_status(PAYMENT=payment_status)
-    pdf.order_number(order_no=unique_id)
-    pdf.get_qr_code(qrcode_dir)
+        pdf = PDF(event_img_filepath=f"artifact/Event/{event_name}/logo/logo.png")
+        pdf.alias_nb_pages()
+        pdf.add_page()
+        pdf.set_font('Times', '', 12)
+        pdf.heading(EVENT_NAME=event_name)
+        pdf.date_time_venue(DATE_TIME_EVENT=date_time_event, VENUE_EVENT=venue_event)
+        pdf.attendee_name(PER_NAME=full_name)
+        pdf.role_in_event(ROLE=role)
+        pdf.payment_status(PAYMENT=payment_status)
+        pdf.order_number(order_no=unique_id)
+        pdf.get_qr_code(qrcode_dir)
 
-    pdf_dir = f"artifact/Event/{event_name}/TicketPDF/{registration_number}.pdf"
+        pdf_dir = f"artifact/Event/{event_name}/TicketPDF/{registration_number}.pdf"
 
-    pdf.output(pdf_dir, 'F')
+        pdf.output(pdf_dir, 'F')
 
-    print(f"{registration_number}-{full_name} PDF saved here:- {pdf_dir}")
+        print(f"{registration_number}-{full_name} PDF saved here:- {pdf_dir}")
+
+        return pdf_dir
+
+    except Exception as e:
+        print(e)
 
 
 def start_entry_process():
@@ -64,40 +68,35 @@ def start_entry_process():
     failed_payment_df = df[df['payment status']=='failed']
 
     for index in range(0,len(confirm_payment_df)):
-        try:
+        # try:
 
-            registration_number = confirm_payment_df['registration_number'].iloc[index].upper()
-            unique_id = confirm_payment_df['order_id'].iloc[index]
-            event_name = confirm_payment_df['payment button title'].iloc[index]
-            email = confirm_payment_df['email'].iloc[index]
-            full_name = confirm_payment_df['full_name'].iloc[index].upper()
-            gender = confirm_payment_df['gender'].iloc[index].upper()
-            role = "ATTENDEE"
-            payment_status = "PAID"
-
-            document = {
-
-                "registration_number": registration_number,
-                "full_name": full_name,
-                "unique_id": unique_id,
-                "event_name": event_name,
-                "email": email,
-                "gender": gender,
-                "role": role,
-                "payment_status":payment_status
-
-            }
-
-            result = collection.insert_one(document)
-
-            qrcode_dir = make_qr(full_name,registration_number,unique_id,event_name)
-
-            generate_pdf(event_name,full_name,role,payment_status,
-                        unique_id,qrcode_dir,registration_number)
+        registration_number = confirm_payment_df['registration_number'].iloc[index].upper()
+        unique_id = confirm_payment_df['order_id'].iloc[index]
+        event_name = confirm_payment_df['payment button title'].iloc[index]
+        email = confirm_payment_df['email'].iloc[index]
+        full_name = confirm_payment_df['full_name'].iloc[index].upper()
+        gender = confirm_payment_df['gender'].iloc[index].upper()
+        role = "ATTENDEE"
+        payment_status = "PAID"
+        document = {
+            "registration_number": registration_number,
+            "full_name": full_name,
+            "unique_id": unique_id,
+            "event_name": event_name,
+            "email": email,
+            "gender": gender,
+            "role": role,
+            "payment_status":payment_status
+        }
+        result = collection.insert_one(document)
+        qrcode_dir = make_qr(full_name,registration_number,unique_id,event_name)
+        pdf_dir = generate_pdf(event_name,full_name,role,payment_status,
+                    unique_id,qrcode_dir,registration_number)
             
+        send_ticket(email,full_name,registration_number,event_name,pdf_dir)
 
-        except:
-            print(f"{registration_number}-{full_name} Already in database")
+        # except:
+        #     print(f"{registration_number}-{full_name} Already in database")
 
         
 
@@ -106,3 +105,21 @@ def start_entry_process():
 # if qr not made then send entries of those to make qr
 # if make pdf from those qr
 # mail those tickcets to everyone
+
+# import pymongo
+
+# # create a MongoClient instance
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# # select a database and a collection
+# db = client["mydatabase"]
+# collection = db["mycollection"]
+
+# # specify the query to find the document to update
+# query = { "name": "John" }
+
+# # specify the new feature to add to the document
+# new_feature = { "$set": { "age": 30 } }
+
+# # update the document with the new feature
+# collection.update_one(query, new_feature)
